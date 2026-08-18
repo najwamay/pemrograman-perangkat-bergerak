14a2fb9a227ab8b7de5d8d10bee5bf13b44fe476
najwamay <najwamay2104@student.uns.ac.id>
2026-08-18 12:10:32 +0700

    first commit

--- dadu/android/app/src/main/kotlin/com/example/dadu/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dadu
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ dadu/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Flutter_2024\\dadu" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter"
+  "PROJECT_DIR=D:\\Flutter_2024\\dadu"
+  "FLUTTER_ROOT=D:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Flutter_2024\\dadu\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Flutter_2024\\dadu"
+  "FLUTTER_TARGET=D:\\Flutter_2024\\dadu\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Flutter_2024\\dadu\\.dart_tool\\package_config.json"
+)
